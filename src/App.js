ab4169058afbf735a26c4a14dce335fe09ec3bc2
islamMaruf <marufislam7424@gmail.com>
2020-04-14 22:47:39 +0600

    route designed for render prop in route component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
     return (
         <BrowserRouter>
             <div className="container">
                 <h1>react router crash course</h1>
+                <Route path="/" render={() => <h1>Hello  i am home route</h1>}/>
+                <Route path="/about" render={() => <h1>Hello i am about route</h1>} />
+                 <Route path="/help" render={() => <h1>Hello i am help route</h1>} />
 
             </div>
         </BrowserRouter>
